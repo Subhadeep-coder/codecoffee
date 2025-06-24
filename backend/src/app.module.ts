925f2b7ecd26cd15c43a90c929573f363ed8b859
Subhadeep-Coder <dharsubhadeep5@gmail.com>
2025-06-24 18:27:16 +0530

    backend project modules initialized

---
+++ backend/src/app.module.ts
@@ -0,0 +1,25 @@
+import { Module } from '@nestjs/common';
+import { AuthModule } from './auth/auth.module';
+import { ProblemsModule } from './problems/problems.module';
+import { UsersModule } from './users/users.module';
+import { ContestsModule } from './contests/contests.module';
+import { SubmissionsModule } from './submissions/submissions.module';
+import { JudgeModule } from './judge/judge.module';
+import { LeaderboardModule } from './leaderboard/leaderboard.module';
+import { ReviewModule } from './review/review.module';
+import { NotificationsModule } from './notifications/notifications.module';
+
+@Module({
+  imports: [
+    AuthModule,
+    ProblemsModule,
+    UsersModule,
+    ContestsModule,
+    SubmissionsModule,
+    JudgeModule,
+    LeaderboardModule,
+    ReviewModule,
+    NotificationsModule
+  ],
+})
+export class AppModule { }
